4e8774ef3f1fe11ea83dac642950d2c0461f21dd
Jonathan cevallos Mayorga <45270300+JonathanCevallos@users.noreply.github.com>
2022-08-16 19:26:31 -0500

    Se agrego lista de pacientes que tiene un cuidador.

--- src/main/java/uteq/api_smart_pills_dispenser/models/Carer.java
+++ src/main/java/uteq/api_smart_pills_dispenser/models/Carer.java
@@ -1,18 +1,14 @@
 package uteq.api_smart_pills_dispenser.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Set;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name = "carer")
 public class Carer {
